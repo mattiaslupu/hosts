58f82f014b978eba626fe6c695de10b74b4bd49f
mattiaslupu <mattias.lupu@yahoo.com>
2026-01-13 16:47:09 +0200

    Am implementat functia shell pentru validare dns cu 3 parametri

--- verificare.sh
+++ verificare.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 INPUT_FILE="/etc/hosts"
+DNS_SERVER="8.8.8.8"
+
+function valideaza_dns {
+    local host=$1
+    local ip=$2
+    local dns=$3
+
+
+    if host "$host" "$dns" | grep -q "$ip"; then
+        echo "[DNS MATCH] $host rezolva la $ip folosind serverul $dns"
+    else
+        echo "[DNS MISMATCH] $host NU rezolva la $ip folosind serverul $dns (sau host inexistent)"
+    fi
+}
+
 
 while read -r line; do
-    if [[ "$line" =~ ^# ]] || [[ -z "$line" ]]; then
+    if [[ "$line" =~ ^# ]] || [[ -z "$line" ]] || [[ "$line" =~ ^127\. ]] || [[ "$line" =~ ^::1 ]]; then
         continue
     fi
 
-    ip_addr=$(echo "$line" | awk '{print $1}')
-    hostname=$(echo "$line" | awk '{print $2}')
+    current_ip=$(echo "$line" | awk '{print $1}')
+    current_host=$(echo "$line" | awk '{print $2}')
 
-    if [[ "$ip_addr" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Format valid: $ip_addr ($hostname)"
-    else
-        echo "Format invalid: $ip_addr"
-    fi
+    valideaza_dns "$current_host" "$current_ip" "$DNS_SERVER"
 
 done < "$INPUT_FILE"
